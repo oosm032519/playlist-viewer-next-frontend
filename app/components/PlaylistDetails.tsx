cd6c289fc085afc3e682f86f49c59a7c1a2142d6
oosm032519 <ryooshima0519@gmail.com>
2024-06-24 19:16:59 +0900

    ソート機能を追加

--- app/components/PlaylistDetails.tsx
+++ app/components/PlaylistDetails.tsx
@@ -1,9 +1,8 @@
 // C:\Users\IdeaProjects\playlist-viewer-next-frontend\app\components\PlaylistDetails.tsx
-// app/components/PlaylistDetails.tsx
 "use client";
 
-import {useMemo} from "react";
-import {useTable, Column, Row} from "react-table";
+import React, {useMemo} from "react";
+import {useTable, useSortBy, Column, Row, HeaderGroup} from "react-table";
 import {Track} from "@/app/types/track";
 import {
     Table,
@@ -13,14 +12,13 @@ import {
     TableHeader,
     TableRow,
 } from "@/app/components/ui/table";
+import {ArrowUpDown} from "lucide-react";
 
 interface PlaylistDetailsProps {
     tracks: Track[];
 }
 
 const PlaylistDetails: React.FC<PlaylistDetailsProps> = ({tracks}) => {
-    console.log("PlaylistDetails: レンダリングされたトラックデータ:", tracks);
-    
     const columns = useMemo<Column<Track>[]>(
         () => [
             {
@@ -40,93 +38,190 @@ const PlaylistDetails: React.FC<PlaylistDetailsProps> = ({tracks}) => {
             {
                 Header: "Danceability",
                 accessor: (row: Track) => row.audioFeatures?.danceability,
-                // value を number | undefined 型に指定
+                sortType: (a: { original: { audioFeatures: { danceability: any; }; }; }, b: { original: { audioFeatures: { danceability: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.danceability;
+                    const bValue = b.original.audioFeatures?.danceability;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Energy",
                 accessor: (row: Track) => row.audioFeatures?.energy,
+                sortType: (a: { original: { audioFeatures: { energy: any; }; }; }, b: { original: { audioFeatures: { energy: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.energy;
+                    const bValue = b.original.audioFeatures?.energy;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Key",
                 accessor: (row: Track) => row.audioFeatures?.key,
-                // value を number | undefined 型に指定
+                sortType: (a: { original: { audioFeatures: { key: any; }; }; }, b: { original: { audioFeatures: { key: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.key;
+                    const bValue = b.original.audioFeatures?.key;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value !== undefined ? value.toString() : "-",
             },
             {
                 Header: "Loudness",
                 accessor: (row: Track) => row.audioFeatures?.loudness,
+                sortType: (a: { original: { audioFeatures: { loudness: any; }; }; }, b: { original: { audioFeatures: { loudness: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.loudness;
+                    const bValue = b.original.audioFeatures?.loudness;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Mode",
                 accessor: (row: Track) => row.audioFeatures?.mode,
-                // value を number | undefined 型に指定
-                Cell: ({value}: { value: number | undefined }) => value !== undefined ? value.toString() : "-",
+                Cell: ({value}: {
+                    value: number | undefined
+                }) => value !== undefined ? (value === 1 ? "MAJOR" : "MINOR") : "-",
             },
             {
                 Header: "Speechiness",
                 accessor: (row: Track) => row.audioFeatures?.speechiness,
+                sortType: (a: { original: { audioFeatures: { speechiness: any; }; }; }, b: { original: { audioFeatures: { speechiness: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.speechiness;
+                    const bValue = b.original.audioFeatures?.speechiness;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Acousticness",
                 accessor: (row: Track) => row.audioFeatures?.acousticness,
+                sortType: (a: { original: { audioFeatures: { acousticness: any; }; }; }, b: { original: { audioFeatures: { acousticness: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.acousticness;
+                    const bValue = b.original.audioFeatures?.acousticness;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Instrumentalness",
                 accessor: (row: Track) => row.audioFeatures?.instrumentalness,
+                sortType: (a: { original: { audioFeatures: { instrumentalness: any; }; }; }, b: { original: { audioFeatures: { instrumentalness: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.instrumentalness;
+                    const bValue = b.original.audioFeatures?.instrumentalness;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Liveness",
                 accessor: (row: Track) => row.audioFeatures?.liveness,
+                sortType: (a: { original: { audioFeatures: { liveness: any; }; }; }, b: { original: { audioFeatures: { liveness: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.liveness;
+                    const bValue = b.original.audioFeatures?.liveness;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Valence",
                 accessor: (row: Track) => row.audioFeatures?.valence,
+                sortType: (a: { original: { audioFeatures: { valence: any; }; }; }, b: { original: { audioFeatures: { valence: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.valence;
+                    const bValue = b.original.audioFeatures?.valence;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Tempo",
                 accessor: (row: Track) => row.audioFeatures?.tempo,
+                sortType: (a: { original: { audioFeatures: { tempo: any; }; }; }, b: { original: { audioFeatures: { tempo: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.tempo;
+                    const bValue = b.original.audioFeatures?.tempo;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value?.toFixed(3) ?? "-",
             },
             {
                 Header: "Duration (ms)",
                 accessor: (row: Track) => row.durationMs,
-                // value を number | undefined 型に指定
+                sortType: (a: { original: { durationMs: any; }; }, b: { original: { durationMs: any; }; }) => {
+                    const aValue = a.original.durationMs;
+                    const bValue = b.original.durationMs;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value !== undefined ? value.toString() : "-",
             },
             {
                 Header: "Time Signature",
                 accessor: (row: Track) => row.audioFeatures?.timeSignature,
-                // value を number | undefined 型に指定
+                sortType: (a: { original: { audioFeatures: { timeSignature: any; }; }; }, b: { original: { audioFeatures: { timeSignature: any; }; }; }) => {
+                    const aValue = a.original.audioFeatures?.timeSignature;
+                    const bValue = b.original.audioFeatures?.timeSignature;
+                    if (aValue === undefined) return 1;
+                    if (bValue === undefined) return -1;
+                    return aValue - bValue;
+                },
                 Cell: ({value}: { value: number | undefined }) => value !== undefined ? value.toString() : "-",
             },
         ],
         []
     );
     
-    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} =
-        useTable({columns, data: tracks});
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable(
+        {
+            columns,
+            data: tracks
+        },
+        useSortBy
+    );
     
     return (
         <Table {...getTableProps()} className="mt-8">
             <TableHeader>
-                {headerGroups.map((headerGroup) => {
-                    const {key, ...restHeaderGroupProps} =
-                        headerGroup.getHeaderGroupProps();
+                {headerGroups.map((headerGroup: HeaderGroup<Track>) => {
+                    const {key, ...restHeaderGroupProps} = headerGroup.getHeaderGroupProps();
                     return (
                         <TableRow key={key} {...restHeaderGroupProps}>
                             {headerGroup.headers.map((column) => {
-                                const {key, ...restColumnProps} = column.getHeaderProps();
+                                const {
+                                    key,
+                                    ...restColumnProps
+                                } = column.getHeaderProps((column as any).getSortByToggleProps());
                                 return (
                                     <TableHead key={key} {...restColumnProps}>
-                                        {column.render("Header")}
+                                        <div className="flex items-center">
+                                            {column.render("Header")}
+                                            <ArrowUpDown className="ml-2 h-4 w-4"/>
+                                        </div>
                                     </TableHead>
                                 );
                             })}
