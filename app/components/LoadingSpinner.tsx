06afb4e2a23b3a740ced8e0ae4744ed76394659c
oosm032519 <ryooshima0519@gmail.com>
2024-07-01 17:39:35 +0900

    LoadingSpinnerのコードフォーマットを修正

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -9,8 +9,10 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({loading}) => {
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black/50 z-50"
-             style={{display: loading ? 'flex' : 'none'}}>
+        <div
+            className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black/50 z-50"
+            style={{display: loading ? "flex" : "none"}}
+        >
             <ClipLoader
                 color="#1DB954"
                 loading={loading}
