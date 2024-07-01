e19c7a9b3ab9e159f2e64a6c70201547f1f4d491
oosm032519 <ryooshima0519@gmail.com>
2024-07-01 18:09:14 +0900

    不要な空白とセミコロンを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,20 @@
-import './globals.css'
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import './globals.css';
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
     title: 'Playlist Viewer Next',
     description: 'View and analyze Spotify playlists',
-}
+};
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className={`${inter.className} bg-gray-dark text-gray-100`}>{children}
+        <body className={`${inter.className} bg-gray-dark text-gray-100`}>
+        {children}
         </body>
-</html>
-)
+        </html>
+    );
 }
