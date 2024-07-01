8f0b179ea823627714523e521a03b5a8e870cf81
oosm032519 <ryooshima0519@gmail.com>
2024-07-01 18:01:50 +0900

    不要なdivタグを削除

--- app/components/PlaylistTable.tsx
+++ app/components/PlaylistTable.tsx
@@ -10,39 +10,36 @@ import {
 
 interface PlaylistTableProps {
     playlists: Playlist[];
-    onPlaylistClick: (playlistId: string) => void; // プロパティとして追加
+    onPlaylistClick: (playlistId: string) => void;
 }
 
 export default function PlaylistTable({playlists, onPlaylistClick}: PlaylistTableProps) {
-    
     return (
-        <div>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Image</TableHead>
-                        <TableHead>Name</TableHead>
-                        <TableHead>Tracks</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {playlists.map((playlist) => (
-                        <TableRow key={playlist.id} onClick={() => onPlaylistClick(playlist.id)}>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Image</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Tracks</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {playlists.map((playlist) => (
+                    <TableRow key={playlist.id} onClick={() => onPlaylistClick(playlist.id)}>
                         <TableCell>
-                                <img
-                                    src={playlist.images[0]?.url}
-                                    alt={playlist.name}
-                                    className="w-12 h-12 object-cover rounded-full"
-                                    width={48}
-                                    height={48}
-                                />
-                            </TableCell>
-                            <TableCell>{playlist.name}</TableCell>
-                            <TableCell>{playlist.tracks.total}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
+                            <img
+                                src={playlist.images[0]?.url}
+                                alt={playlist.name}
+                                className="w-12 h-12 object-cover rounded-full"
+                                width={48}
+                                height={48}
+                            />
+                        </TableCell>
+                        <TableCell>{playlist.name}</TableCell>
+                        <TableCell>{playlist.tracks.total}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     );
 }
