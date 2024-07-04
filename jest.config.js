cc6c136ae2bfd16be1657ed306c7fd0a56737555
oosm032519 <ryooshima0519@gmail.com>
2024-07-05 02:38:42 +0900

    設定ファイルの改善

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,16 @@ const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     silent: false,
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.jest.json'
+        }
+    }
 }
 
 module.exports = createJestConfig(customJestConfig)
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import {toHaveNoViolations} from 'jest-axe';
 import 'jest-fetch-mock';
+import '@jest/globals';
 
 expect.extend(toHaveNoViolations);
