5edc712fc75504bc5541aaa839c0dbadc48a0def
oosm032519 <ryooshima0519@gmail.com>
2024-07-23 20:16:48 +0900

    ログインリダイレクトを修正

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,11 +1,9 @@
 // app/api/login/route.ts
 import {NextResponse} from 'next/server';
 
-const BACKENDURL = process.env.BACKEND_URL || 'http://localhost:8080';
-const APIURL = `${BACKENDURL}/oauth2/authorization/spotify`;
-
 export async function GET() {
-    console.log('Using BACKEND_URL:', BACKENDURL);
-    console.log('Redirecting to Spotify login:', APIURL);
-    return NextResponse.redirect(APIURL);
+    const BACKENDURL = process.env.BACKEND_URL;
+    const redirectUrl = `${BACKENDURL}/oauth2/authorization/spotify`;
+    
+    return NextResponse.json({redirectUrl});
 }
