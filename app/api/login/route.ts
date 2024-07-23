d880915960619b06f63da703611e132992af92b0
oosm032519 <ryooshima0519@gmail.com>
2024-07-23 20:05:36 +0900

    ワークフローにデプロイ操作を追加

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,6 +5,5 @@ const BACKENDURL = process.env.BACKEND_URL || 'http://localhost:8080';
 const APIURL = `${BACKENDURL}/oauth2/authorization/spotify`;
 
 export async function GET() {
-    const loginUrl = process.env.NEXT_PUBLIC_SPOTIFY_AUTH_URL || APIURL;
-    return NextResponse.redirect(loginUrl);
+    return NextResponse.redirect(APIURL);
 }
