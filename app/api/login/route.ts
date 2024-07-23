0f115843746974f19d89d0c29570c9606d927a3b
oosm032519 <ryooshima0519@gmail.com>
2024-07-23 20:09:55 +0900

    ログを追加

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,5 +5,7 @@ const BACKENDURL = process.env.BACKEND_URL || 'http://localhost:8080';
 const APIURL = `${BACKENDURL}/oauth2/authorization/spotify`;
 
 export async function GET() {
+    console.log('Using BACKEND_URL:', BACKENDURL);
+    console.log('Redirecting to Spotify login:', APIURL);
     return NextResponse.redirect(APIURL);
 }
