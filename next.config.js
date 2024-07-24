42bcaa3d9e59ee6c7c62aaa530a51cee44618d0b
oosm032519 <ryooshima0519@gmail.com>
2024-07-24 12:55:11 +0900

    リダイレクトを調整

--- next.config.js
+++ next.config.js
@@ -30,14 +30,6 @@ const nextConfig = {
             },
         ];
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: '${process.env.BACKEND_URL}/api/:path*',
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
