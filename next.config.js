e2e39d2bb97e388dee64326fce61add531bf7055
oosm032519 <ryooshima0519@gmail.com>
2024-08-02 12:26:24 +0900

    rewrites設定を削除

--- next.config.js
+++ next.config.js
@@ -39,15 +39,6 @@ const nextConfig = {
             },
         ];
     },
-
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8080/api/:path*',
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
