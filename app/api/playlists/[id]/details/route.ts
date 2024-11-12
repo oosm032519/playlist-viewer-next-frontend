200408dcdcd903c00370ac4dd290f4e08001c6cb
oosm032519 <ryooshima0519@gmail.com>
2024-11-12 20:39:03 +0900

    fix: すべてのAPIルートでsendRequestとhandleApiErrorを使用するよう変更

--- app/api/playlists/[id]/details/route.ts
+++ app/api/playlists/[id]/details/route.ts
@@ -1,26 +1,15 @@
 // api/playlists/[id]/details/route.ts
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
-import {handleApiError} from '@/app/lib/api-utils';
+import {getCookies, handleApiError, sendRequest} from '@/app/lib/api-utils';
 
 export async function GET(req: NextRequest, {params}: { params: { id: string } }) {
     const {id} = params;
     try {
-        const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
-        const response = await fetch(`${backendUrl}/api/playlists/${id}/details`, {
-            credentials: 'include',
-        });
-        
-        if (!response.ok) {
-            const errorData = await response.json();  // エラーメッセージがあれば取得
-            return new NextResponse(JSON.stringify(errorData), {
-                status: response.status,
-                headers: {'Content-Type': 'application/json'}
-            });
-        }
+        const response = await sendRequest(`/api/playlists/${id}/details`, 'GET', undefined, getCookies(req));
         
         const data = await response.json();
-        return new NextResponse(JSON.stringify(data), {
+        return NextResponse.json(data, {
             status: 200,
             headers: {'Content-Type': 'application/json'}
         });
--- app/api/playlists/recommendations/route.ts
+++ app/api/playlists/recommendations/route.ts
@@ -1,7 +1,7 @@
 // app/api/playlists/recommendations/route.ts
 
 import {NextResponse, NextRequest} from 'next/server';
-import {handleApiError} from '@/app/lib/api-utils';
+import {getCookies, handleApiError, sendRequest} from '@/app/lib/api-utils';
 
 // リクエストボディの型定義
 interface RecommendationRequest {
@@ -14,25 +14,10 @@ export async function POST(request: NextRequest) {
     try {
         const requestBody: RecommendationRequest = await request.json();
         
-        const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
-        const response = await fetch(`${backendUrl}/api/playlists/recommendations`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(requestBody),
-            credentials: 'include',
-        });
-        
-        if (!response.ok) {  // レスポンスが失敗した場合
-            const errorData = await response.json(); // エラーレスポンスをJSONとして解析
-            console.error("Fetch error:", response.status, errorData);
-            return new NextResponse(JSON.stringify(errorData), { // エラーレスポンスを返す
-                status: response.status,
-                headers: {'Content-Type': 'application/json'}
-            });
-        }
+        const response = await sendRequest('/api/playlists/recommendations', 'POST', requestBody, getCookies(request));
         
         const data = await response.json();
-        return new NextResponse(JSON.stringify(data), {
+        return NextResponse.json(data, {
             status: 200,
             headers: {'Content-Type': 'application/json'}
         });
