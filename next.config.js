633e17d64d4cc2e82b6cbb5712214bb97ea49f98
oosm032519 <ryooshima0519@gmail.com>
2025-01-20 18:20:59 +0900

    feat: プレースホルダーを許可

--- next.config.js
+++ next.config.js
@@ -19,7 +19,13 @@ const nextConfig = {
                 hostname: '*.spotifycdn.com',
                 port: '',
                 pathname: '/**'
-            }
+            },
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+                port: '',
+                pathname: '/**',
+            },
         ],
         minimumCacheTTL: 60,
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
